c1b630723e420f0d61ac7ed4ad4d2548e4f56fc0
Pierre Chevalier <pierrrechevalier83@gmail.com>
2025-01-18 18:33:34 +0000

    Write firmware for the effortless

---
+++ keyboards/handwired/pierrec/effortless/config.h
@@ -0,0 +1,16 @@
+// Copyright 2023 Pierre Chevalier (@pierrec83)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define EE_HANDS
+
+#define DIRECT_PINS {{GP29,     GP15,   GP10, GP7, GP3}, \
+                     {GP28,     GP14,    GP9, GP6, GP2}, \
+                     {GP27,     GP13,    GP8, GP5, GP1}, \
+                     {NO_PIN, NO_PIN, NO_PIN, GP4, GP0}}
+
+#define DIRECT_PINS_RIGHT {{GP29,  GP15,   GP10,    GP7,    GP3}, \
+                           {GP28,  GP14,    GP9,    GP6,    GP2}, \
+                           {GP27,  GP13,    GP8,    GP5,    GP1}, \
+                           {GP26,  GP11, NO_PIN, NO_PIN, NO_PIN}}
