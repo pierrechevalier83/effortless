4c448cb1c0fa9716d996417f0692eadea965a07c
Pierre Chevalier <pierrrechevalier83@gmail.com>
2025-03-02 12:47:16 +0000

    Move PCB 2mm to the left by adjusting PCB_X_OFFSET

--- 3d_model/src/params.rs
+++ 3d_model/src/params.rs
@@ -13,7 +13,7 @@ pub(crate) const KEYCAP_XYZ: DVec3 = DVec3::new(17.5, 16.5, 8.);
 pub(crate) const BASE_Z_OFFSET: f64 = 10.0;
 
 // PCB positioning
-pub(crate) const PCB_X_OFFSET: f64 = -1.1;
+pub(crate) const PCB_X_OFFSET: f64 = -3.1;
 
 // Number of switches
 pub(crate) const NUM_COLS: usize = 5;
